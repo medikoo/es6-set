b3c805aef24df46a340811427184ae8c6b06f9df
Mariusz Nowak <mariusz@medikoo.com>
2014-01-23 10:12:37 +0100

    Do no produce special setData objects
    Remove support for comparator argument (as in updated spec)
    Make __setData__ property configurable true, it'll open door for possible
    performance hacking

--- polyfill.js
+++ polyfill.js
@@ -1,33 +1,31 @@
 'use strict';
 
-var setPrototypeOf = require('es5-ext/object/set-prototype-of')
+var clear          = require('es5-ext/array/#/clear')
+  , eIndexOf       = require('es5-ext/array/#/e-index-of')
+  , setPrototypeOf = require('es5-ext/object/set-prototype-of')
   , callable       = require('es5-ext/object/valid-callable')
   , d              = require('d/d')
   , ee             = require('event-emitter/lib/core')
   , Symbol         = require('es6-symbol')
   , iterator       = require('es6-iterator/valid-iterable')
   , forOf          = require('es6-iterator/for-of')
-  , getSetData     = require('./_get-set-data')
   , Iterator       = require('./_iterator')
   , isNative       = require('./is-native-implemented')
 
   , call = Function.prototype.call, defineProperty = Object.defineProperty
   , SetPoly, getValues;
 
-module.exports = SetPoly = function (/*iterable, comparator*/) {
-	var iterable = arguments[0], comparator = arguments[1];
-	if (!(this instanceof SetPoly)) return new SetPoly(iterable, comparator);
+module.exports = SetPoly = function (/*iterable*/) {
+	var iterable = arguments[0];
+	if (!(this instanceof SetPoly)) return new SetPoly(iterable);
 	if (this.__setData__ !== undefined) {
 		throw new TypeError(this + " cannot be reinitialized");
 	}
 	if (iterable != null) iterator(iterable);
-	if (comparator !== undefined) {
-		if (comparator !== 'is') throw new RangeError("Unknown comparator");
-	}
-	defineProperty(this, '__setData__', d('', getSetData(comparator)));
+	defineProperty(this, '__setData__', d('c', []));
 	if (!iterable) return;
 	forOf(iterable, function (value) {
-		if (this.eIndexOf(value) !== -1) return;
+		if (eIndexOf.call(this, value) !== -1) return;
 		this.push(value);
 	}, this.__setData__);
 };
@@ -47,11 +45,11 @@ ee(Object.defineProperties(SetPoly.prototype, {
 	}),
 	clear: d(function () {
 		if (!this.__setData__.length) return;
-		this.__setData__.clear();
+		clear.call(this.__setData__);
 		this.emit('_clear');
 	}),
 	delete: d(function (value) {
-		var index = this.__setData__.eIndexOf(value);
+		var index = eIndexOf.call(this.__setData__, value);
 		if (index === -1) return false;
 		this.__setData__.splice(index, 1);
 		this.emit('_delete', index, value);
@@ -70,7 +68,7 @@ ee(Object.defineProperties(SetPoly.prototype, {
 		}
 	}),
 	has: d(function (value) {
-		return (this.__setData__.eIndexOf(value) !== -1);
+		return (eIndexOf.call(this.__setData__, value) !== -1);
 	}),
 	keys: d(getValues = function () { return this.values(); }),
 	size: d.gs(function () { return this.__setData__.length; }),
