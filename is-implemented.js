90759a91648ce74eb81c30f3e6c12c28f2e390eb
Mariusz Nowak <mariusz@medikoo.com>
2014-10-10 15:53:41 +0200

    Pass only extendable implementation of sets

--- is-implemented.js
+++ is-implemented.js
@@ -1,7 +1,11 @@
 'use strict';
 
+var d = require('d')
+
+  , create = Object.create, setPrototypeOf = Object.setPrototypeOf;
+
 module.exports = function () {
-	var set, iterator, result;
+	var set, iterator, result, SubSet;
 	if (typeof Set !== 'function') return false;
 	if (String(Set.prototype) !== '[object Set]') return false;
 	set = new Set(['raz', 'dwa', 'trzy']);
@@ -19,5 +23,22 @@ module.exports = function () {
 	result = iterator.next();
 	if (result.done !== false) return false;
 	if (result.value !== 'raz') return false;
+
+	// Non new call
+	try { set = Set([1, 2, 3]); } catch (e) { return false; } //jslint: ignore
+
+	if (!set) return false;
+	if (set.size !== 3) return false;
+
+	// Extendable
+	if (setPrototypeOf) {
+		SubSet = function () { Set.apply(this, arguments); };
+		setPrototypeOf(SubSet, Set);
+		SubSet.prototype = create(Set.prototype, { constructor: d(SubSet) });
+		try { set = new SubSet([1, 2, 3]); } catch (e) { return false; }
+		try {
+			if (set.size !== 3) return false;
+		} catch (e) { return false; }
+	}
 	return true;
 };
